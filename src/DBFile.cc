e13ecae6af0e1da870269755e56057e76ea6c480
SoulKeeper7 <amitkagarwal@ufl.edu>
2018-02-10 04:11:46 -0500

    all test cases working

--- src/DBFile.cc
+++ src/DBFile.cc
@@ -3,92 +3,70 @@
 
 #include "DBFile.h"
 #include "HeapFile.h"
-
+#include "GenericDBFileBaseClass.h"
+//#include "SortedFile.h"
 
 using std::string;
 using std::ifstream;
 using std::ofstream;
 
-int DBFile::Create (const char* fpath, fType ftype, void* startup) {
-  //FATALIF(db, "File already opened.");
-  createFile(ftype);
-  return db->Create(fpath, startup);
+int DBFile::Create(const char* fpath, fType ftype, void* startup) {
+	//FATALIF(db, "File already opened.");
+	//createFile(ftype);
+	Genricdb = new HeapFile;
+	return Genricdb->Create(fpath, startup);
 }
 
-int DBFile::Open (const char* fpath) {
-  //FATALIF(db, "File already opened.");
-  int ftype = heap;  // use heap file by default
-  string metaFile = DBFileBase::getTableName(fpath)+".meta";
-  ifstream ifs(metaFile.c_str());
-  if (ifs) {
-    ifs >> ftype;  // the first line contains file type
-    ifs.close();
-  }
-  createFile(static_cast<fType>(ftype));
-  return db->Open(fpath);
+int DBFile::Open(const char* fpath) {
+	//FATALIF(db, "File already opened.");
+	//int ftype = heap;  // use heap file by default
+	//string metaFile = GenericDBFileBaseClass::getTableName(fpath) + ".meta";
+	//ifstream ifs(metaFile.c_str());
+	//if (ifs) {
+	//	ifs >> ftype;  // the first line contains file type
+	//	ifs.close();
+	//}
+	//createFile(static_cast<fType>(ftype));
+	Genricdb = new HeapFile;
+	return Genricdb->Open(fpath);
 }
 
-void DBFile::createFile(fType ftype) {
-  switch (ftype) {
-    case heap: db = new HeapFile; break;
-   // case SORTED: db = new SortedFile; break;
-    default: db = NULL;
-  }
-  //FATALIF(db==NULL, "Invalid file type.");
-}
+/*void DBFile::createFile(fType ftype) {
+	switch (ftype) {
+	case heap: Genricdb = new HeapFile;
+		break;
+		//case SORTED: db = new SortedFile; break;
+	
+	}
+	//FATALIF(db == NULL, "Invalid file type.");
+}*/
 
 int DBFile::Close() {
-  return db->Close();
+	return Genricdb->Close();
 }
 
-void DBFile::Add (Record& addme) {
-  return db->Add(addme);
+void DBFile::Add(Record& addme) {
+	return Genricdb->Add(addme);
 }
 
-void DBFile::Load (Schema& myschema, char* loadpath) {
-  return db->Load(myschema, loadpath);
+void DBFile::Load(Schema& myschema, char* loadpath) {
+	return Genricdb->Load(myschema, loadpath);
 }
 
 void DBFile::MoveFirst() {
-  return db->MoveFirst();
+	return Genricdb->MoveFirst();
 }
 
-int DBFile::GetNext (Record& fetchme) {
-  return db->GetNext(fetchme);
+int DBFile::GetNext(Record& fetchme) {
+	return Genricdb->GetNext(fetchme);
 }
 
-int DBFile::GetNext (Record& fetchme, CNF& cnf, Record& literal) { 
-  return db->GetNext(fetchme, cnf, literal);
+int DBFile::GetNext(Record& fetchme, CNF& cnf, Record& literal) {
+	return Genricdb->GetNext(fetchme, cnf, literal);
 }
 
-DBFile::DBFile(): db(NULL) {}
-DBFile::~DBFile() { delete db; }
+DBFile::DBFile() {}
+DBFile::~DBFile() { }//delete Genricdb; }
 
-int DBFileBase::Create(const char* fpath, void* startup) {
-  theFile.Open(0, fpath);
-  return 1;
-}
 
-int DBFileBase::Open (const char* fpath) {
-  theFile.Open(1, fpath);
-  return 1;
-}
 
-void DBFileBase::Load (Schema& myschema, char* loadpath) {
-  startWrite();
-  FILE* ifp = fopen(loadpath, "r");
-  //FATALIF(ifp==NULL, loadpath);
-
-  Record next;
-  curPage.EmptyItOut();  // creates the first page
-  while (next.SuckNextRecord(&myschema, ifp)) Add(next);
-  theFile.addPage(&curPage);  // writes the last page  -- added in close()
-}
-
-int DBFileBase::GetNext (Record& fetchme) {
-  while (!curPage.GetFirst(&fetchme)) {
-    if(++curPageIdx > theFile.lastIndex()) return 0;  // no more records
-    theFile.GetPage(&curPage, curPageIdx);
-  }
-  return 1;
-}
--- src/HeapFile.h
+++ src/HeapFile.h
@@ -1,40 +1,57 @@
-#ifndef HEAP_FILE_H_
-#define HEAP_FILE_H_
+#ifndef HEAP_FILE_H
+#define HEAP_FILE_H
 
 #include "DBFile.h"
+#include "GenericDBFileBaseClass.h"
 
-class HeapFile : protected DBFileBase {
-	friend class DBFile;
-	friend class SortedFile;   // sort file used a temp heap file for merging
-	using DBFileBase::GetNext;
-protected:
+class HeapFile : virtual public GenericDBFileBaseClass {
+	//friend class DBFile;
+	//friend class SortedFile;   // sort file used a temp heap file for merging
+	//using GenericDBFileBaseClass::GetNext;
+
+private:
+	File * theFile;
+	Page *curPage; //Record writes go into this page
+	//Page *read_page;  //This page is only for reading
+	int curPageIdx;     //Current Page being read. 0 means no pages to read
+	//bool dirty;       //If true, current page being read is dirty(Not yet written to disk). 
+	fType type;
+public:
 	HeapFile();
-	~HeapFile() {}
+	~HeapFile() ;
 
 	int Close();
 	void Add(Record& me);
 
 	void MoveFirst();
 	int GetNext(Record& fetchme, CNF& cnf, Record& literal);
+	int GetNext(Record& fetchme);
 
-protected:
-	void startWrite() { mode = WRITE; }
-	void startRead();
+	int Open(const char * f_path);
 
-private:
+	int Create( const char * fpath, void * startup);
+
+
+	void Load(Schema & myschema, char * loadpath);
+
+//protected:
+	//void startWrite(); //{ mode = WRITE; }
+	//void startRead();
+
+/*private:
 	void addtoNewPage(Record& rec) {
-		curPage.EmptyItOut();
-		curPage.Append(&rec);
-	}
+		curPage->EmptyItOut();
+		curPage->Append(&rec);
+	}*/
 
-	HeapFile(const HeapFile&);
-	HeapFile& operator=(const HeapFile&);
+	//HeapFile(const HeapFile&);
+	//HeapFile& operator=(const HeapFile&);
 };
 
-inline void HeapFile::startRead() {
-	if (mode == READ) return;
-	mode = READ;
+/*inline void HeapFile::startRead() {
+	//if (mode == READ) return;
+	//mode = READ;
 	 theFile.AddPage(&curPage,0);
-}
+}*/
 
 #endif
